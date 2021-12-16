ae255a71117d1b6f814130ccb8ce221eac4a2ffc
DESKTOP-9RO8RKP\infinity <baldoswill@gmail.com>
2021-12-16 09:46:37 +0800

    Moved header and footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { useInView } from 'react-intersection-observer'
+// import { BrowserRoutes } from "react-router-dom";
 
 function App() {
+
+  const [ref, inView] = useInView({
+    threshold: .9,
+  });
+
   return (
     <>
+      <Header isInview={inView} propRef={ref} />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,25 @@
 import React from "react";
-import { useInView } from 'react-intersection-observer'
-import Header from "../../components/Header/Header";
+
+// import Header from "../../components/Header/Header";
 import Favorites from "../../components/Favorites/Favorites";
 import Services from "../../components/Services/Services";
 import About from "../../components/About/About";
 import News from "../../components/News/News";
 import Contact from "../../components/Contact/Contact";
-import Footer from "../../components/Footer/Footer";
+// import Footer from "../../components/Footer/Footer";
+// import { BrowserRouter, Routes, Switch } from "react-router-dom";
 
 export default function Home() {
 
-  const [ref, inView] = useInView({
-    threshold: .9,
-  });
-
   return (
     <>
-      <Header isInview={inView} propRef={ref} />
+
       <Favorites />
       <Services />
       <About />
       <News />
       <Contact />
-      <Footer />
+
     </>
   );
 }
